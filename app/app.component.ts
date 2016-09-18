1e5d1bb3a0b69363c2e0ddac38be85d59a58eac7
aldavies <aldavies@umich.edu>
2016-09-18 01:17:28 -0400

    building components

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <div [style.color]="chagiColor">My cute, fat, fluffy chagi {{name}}!!</div>
+   	<button (click)="makeChagiSleep()">Chagi's magic button</button>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+	//[means property binding]
+	//(means event binding)
+	title = 'Customer App';
+	name = 'Tina';
+	chagiColor = 'pink';
+
+	makeChagiSleep() {
+		this.chagiColor = this.chagiColor === 'blue' ? 'pink' : 'blue';
+	}
+}
